339f5ce3bdad735e2a9a5488881a11728391f651
thenrymunyoki <32766747+thenrymunyoki@users.noreply.github.com>
2023-07-26 23:26:13 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,14 +3,22 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
-// Serve the static frontend files
-app.use(express.static(path.join(__dirname, "build")));
+/ Serve the static frontend files from the 'build' folder
+app.use(express.static(path.join(__dirname, "../my-react-app/build")));
+
+// Serve specific files directly (index.js and App.js in this case)
+app.get("/index.js", (req, res) => {
+  res.sendFile(path.join(__dirname, "../my-react-app/build", "index.js"));
+});
+
+app.get("/App.js", (req, res) => {
+  res.sendFile(path.join(__dirname, "../my-react-app/build", "App.js"));
+});
 
 // For any other routes, serve the index.html file
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "../my-react-app/build", "index.html"));
 });
-
 // Start the server
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
