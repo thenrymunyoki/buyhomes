b06707204e5737cb47ed1f82bc5ea007ff27fab7
thenrymunyoki <32766747+thenrymunyoki@users.noreply.github.com>
2023-07-29 21:49:33 +0300

    Update custom-build-script.js

--- custom-build-script.js
+++ custom-build-script.js
@@ -1,18 +1,20 @@
-const fs = require('fs-extra');
-const path = require('path');
-
-const sourceDir = path.resolve(__dirname, 'src');
-const targetDir = path.resolve(__dirname, 'dist');
+const webpack = require('webpack');
+const webpackConfig = require('./webpack.config.js'); // Replace 'webpack.config.js' with the name of your webpack configuration file if different
 
 async function build() {
   try {
-    // Clear the target directory if it already exists
-    await fs.emptyDir(targetDir);
-
-    // Copy the contents of the source directory to the target directory
-    await fs.copy(sourceDir, targetDir);
-
-    console.log('Build completed successfully!');
+    // Run webpack with the specified configuration
+    await new Promise((resolve, reject) => {
+      webpack(webpackConfig, (err, stats) => {
+        if (err || stats.hasErrors()) {
+          console.error('Error during build:', err || stats.toString());
+          process.exit(1);
+        } else {
+          console.log('Build completed successfully!');
+          resolve();
+        }
+      });
+    });
   } catch (err) {
     console.error('Error during build:', err);
     process.exit(1);
