80e9e22fd80b68853e9a3f69f45ddfe72021f1fc
thenrymunyoki <32766747+thenrymunyoki@users.noreply.github.com>
2023-07-29 22:42:23 +0300

    Update webpack.config.js

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js', // Replace 'index.js' with your React application's entry point
+  entry: path.resolve(__dirname, 'src', 'index.js'), // Update the entry point path
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
